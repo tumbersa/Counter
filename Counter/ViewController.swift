e217dfe2f0410da8d01c34ff39f73a5693c13214
gleb_kapustin <gleb.kapustin.98@mail.ru>
2023-07-22 21:33:26 +0300

    Усложнённая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,19 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var counterValueLabel: UILabel!
-    @IBOutlet weak var clickOnMeButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var workDisplayTextView: UITextView!
+    
     var value = 0
-    @IBAction func changeCounterValueLabel(_ sender: Any) {
+    
+    func timeСalculation()->String {
+        let date = DateFormatter()
+        date.dateFormat = "HH:mm E, d MMM y"
+        return date.string(from: Date.now)
+    }
+    
+    @IBAction func plusOne(_ sender: Any) {
         value += 1
         counterValueLabel.text = "Значение счётчика: \(value)"
+        workDisplayTextView.text += "\(timeСalculation()): значение изменено на +1\n"
     }
+    
+    @IBAction func minusOneOrNothing(_ sender: Any) {
+        if value != 0 {
+            value -= 1
+            counterValueLabel.text = "Значение счётчика: \(value)"
+            workDisplayTextView.text += "\(timeСalculation()): значение изменено на -1\n"
+        } else {
+            workDisplayTextView.text += "\(timeСalculation()): попытка уменьшить значение счётчика ниже 0\n"
+        }
+    }
+    
+    @IBAction func refreshValue(_ sender: Any) {
+        value = 0
+        counterValueLabel.text = "Значение счётчика: \(value)"
+        workDisplayTextView.text += "\(timeСalculation()): значение сброшено\n"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        workDisplayTextView.isEditable = false
+        workDisplayTextView.text = "История изменений:\n"
     }
-
-
+    
 }
 
