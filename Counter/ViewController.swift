c1d5a3a2929bc717b04269d793e7174d9c4ffd98
gleb_kapustin <gleb.kapustin.98@mail.ru>
2023-07-23 22:56:54 +0300

    Добавлено: Модификаторы доступа к свойствам и методам, кроме override func viewDidLoad() Структурированное расположение свойств и методов в классе ViewController Автоматический скроллинг при изменении workDisplayTextView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,43 +7,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var counterValueLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var refreshButton: UIButton!
-    @IBOutlet weak var workDisplayTextView: UITextView!
+    @IBOutlet private weak var counterValueLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var refreshButton: UIButton!
+    @IBOutlet private weak var workDisplayTextView: UITextView!
     
-    var value = 0
-    
-    func timeСalculation()->String {
-        let date = DateFormatter()
-        date.dateFormat = "HH:mm E, d MMM y"
-        return date.string(from: Date.now)
-    }
-    
-    @IBAction func plusOne(_ sender: Any) {
-        value += 1
-        counterValueLabel.text = "Значение счётчика: \(value)"
-        workDisplayTextView.text += "\(timeСalculation()): значение изменено на +1\n"
-    }
-    
-    @IBAction func minusOneOrNothing(_ sender: Any) {
-        if value != 0 {
-            value -= 1
-            counterValueLabel.text = "Значение счётчика: \(value)"
-            workDisplayTextView.text += "\(timeСalculation()): значение изменено на -1\n"
-        } else {
-            workDisplayTextView.text += "\(timeСalculation()): попытка уменьшить значение счётчика ниже 0\n"
-        }
-    }
-    
-    @IBAction func refreshValue(_ sender: Any) {
-        value = 0
-        counterValueLabel.text = "Значение счётчика: \(value)"
-        workDisplayTextView.text += "\(timeСalculation()): значение сброшено\n"
-    }
+    private var value = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,5 +24,45 @@ class ViewController: UIViewController {
         workDisplayTextView.text = "История изменений:\n"
     }
     
+    private func timeСalculation()->String {
+        let date = DateFormatter()
+        date.dateFormat = "HH:mm E, d MMM y"
+        return date.string(from: Date.now)
+    }
+    
+    private func scrollTextViewToBottom(textView: UITextView) {
+        if textView.text.count > 0 {
+            let location = textView.text.count - 1
+            let bottom = NSMakeRange(location, 1)
+            textView.scrollRangeToVisible(bottom)
+        }
+    }
+    @IBAction private func plusOne(_ sender: Any) {
+        value += 1
+        counterValueLabel.text = "Значение счётчика: \(value)"
+        workDisplayTextView.text += "\(timeСalculation()): значение изменено на +1\n"
+        scrollTextViewToBottom(textView: workDisplayTextView)
+    }
+    
+    @IBAction private func minusOneOrNothing(_ sender: Any) {
+        if value != 0 {
+            value -= 1
+            counterValueLabel.text = "Значение счётчика: \(value)"
+            workDisplayTextView.text += "\(timeСalculation()): значение изменено на -1\n"
+        } else {
+            workDisplayTextView.text += "\(timeСalculation()): попытка уменьшить значение счётчика ниже 0\n"
+        }
+        scrollTextViewToBottom(textView: workDisplayTextView)
+    }
+    
+    @IBAction private func refreshValue(_ sender: Any) {
+        value = 0
+        counterValueLabel.text = "Значение счётчика: \(value)"
+        workDisplayTextView.text += "\(timeСalculation()): значение сброшено\n"
+        scrollTextViewToBottom(textView: workDisplayTextView)
+    }
+    
+    
 }
+
 
